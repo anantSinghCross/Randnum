0a17f56849c603742500732fc661dc81d025d71e
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2019-11-04 14:28:59 +0530

    Initial commit

---
+++ app/src/main/java/com/crossbox/randnum/MainActivity.java
@@ -0,0 +1,35 @@
+package com.crossbox.randnum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView t1;
+    MaterialButton mb1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        t1 = findViewById(R.id.num);
+        mb1 = findViewById(R.id.mb1);
+
+        mb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rand = new Random();
+                int num = rand.nextInt(10);
+                t1.setText(String.valueOf(num));
+            }
+        });
+    }
+}
